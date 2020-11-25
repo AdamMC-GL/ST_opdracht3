0d77d02b047291e95e02297f908117ba982131bf
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2020-11-25 13:07:17 +0100

    Add files via upload

---
+++ ST_opdracht3/Touch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class Touch : Sense
+{
+    void OnTriggerEnter(Collider other)
+    {
+        aspect aspect = other.GetComponent<aspect>();
+        if (aspect != null)
+        {
+            //Check the aspect
+            if (aspect.aspectType != aspectName)
+            {
+                print("Enemy Touch Detected");
+            }
+        }
+    }
+}
---
+++ ST_opdracht3/aspect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class aspect : MonoBehaviour
+{
+    public enum AspectTypes
+    {
+        PLAYER,
+        ENEMY,
+    }
+    public AspectTypes aspectType;
+}
